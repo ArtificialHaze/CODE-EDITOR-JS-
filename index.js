39018e4d2e9a857727743d776fefe0ce2c98d9b9
ArtificialHaze <83704626+ArtificialHaze@users.noreply.github.com>
2022-11-03 17:21:31 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+  const [input, output] = document.querySelectorAll(".codemirror-textarea");
+  const [run, clear] = document.querySelectorAll("button");
+
+  const editor = CodeMirror.fromTextArea(input, { lineNumbers: true });
+  const shell = CodeMirror.fromTextArea(output, { lineNumbers: false });
+
+  run.addEventListener("click", () => {
+    const codeToRun = editor.getValue();
+    try {
+      shell.replaceRange(
+        "$ " + eval(`${codeToRun}`) + "\n",
+        CodeMirror.Pos(shell.lastLine())
+      );
+    } catch (e) {
+      shell.replaceRange("$ " + e + "\n", CodeMirror.Pos(shell.lastLine()));
+    }
+  });
+
+  clear.addEventListener("click", () => shell.setValue(""));
+};
